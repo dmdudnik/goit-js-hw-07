8a42546623f998ebb44be6ac834a7e918b8a9ffc
Dmitriy Dudnik <ddudnik79@gmail.com>
2021-01-27 00:00:03 +0200

    исправлено задание 1

--- js/task-1.js
+++ js/task-1.js
@@ -3,15 +3,15 @@ console.log(`В списке ${categorySum} категории`);
 
 const title = document.querySelectorAll("h2");
 const elementSum = document.querySelectorAll(".item");
-console.log(`Категория:  ${title[0].textContent}`);
+console.log(`Категория: ${title[0].textContent}`);
 console.log(
   `Количество элементов: ${elementSum[0].lastElementChild.children.length}`
 );
-console.log(`Категория:  ${title[1].textContent}`);
+console.log(`Категория: ${title[1].textContent}`);
 console.log(
   `Количество элементов: ${elementSum[1].lastElementChild.children.length}`
 );
-console.log(`Категория:  ${title[2].textContent}`);
+console.log(`Категория: ${title[2].textContent}`);
 console.log(
   `Количество элементов: ${elementSum[2].lastElementChild.children.length}`
 );
